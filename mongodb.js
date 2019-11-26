5776a3cdee19b1b0f97b8d5b55fe9d552b039b87
Ibrahim SALAWUDEEN <isalawudeen@cscs.ng>
2019-11-26 17:43:54 +0100

    initial commit

--- mongodb.js
+++ mongodb.js
@@ -10,8 +10,22 @@ mongoClient.connect( connectionURL, {useNewUrlParser : true,  useUnifiedTopology
     } 
     
     const db = client.db(databaseName)
-    db.collection('user').insertOne({
-        name : 'ibrahim',
-        age : 29
+    db.collection('tasks').insertMany([
+        {
+            description : 'clean the house',
+            completed : true
+        },
+        {
+            description : 'Sweep the room',
+            completed : true
+        },
+        {
+            description : 'wash the car',
+            completed : false
+        }
+    ], (error, result) => {
+        if(error) return console.log('Could not insert into db')
+
+        console.log(result.ops)
     })
 })
